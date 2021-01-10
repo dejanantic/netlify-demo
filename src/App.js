5210e19f5a2fb43e196f2758f113fca0dafe697e
Dejan Antic <dex.antic@gmail.com>
2021-01-10 20:34:47 +0100

    Add my message

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Successful deployment to Netlify!
+          <span className="emoji" role="img" aria-label="high five">
+            ✋
+          </span>
+        </h1>
       </header>
     </div>
   );
